77db0f183916e988832b4d996e0625f231eac6a9
Lmy20020802 <105573171+Lmy20020802@users.noreply.github.com>
2022-12-03 20:15:25 +0800

    1.项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    <!--头部区域-->
+    <div style="height: 60px;line-height: 60px;background-color:white;">
+      <img src="@/assets/logo.png" style="width: 40px;height: 40px;position: relative; top: 10px;left: 20px">
+      <span style="margin-left: 25px;font-size: 24px">图书管理系统</span>
+    </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
