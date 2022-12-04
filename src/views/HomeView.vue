544e5491cd552a3b34bd6277c4f19810931a3341
Lmy20020802 <105573171+Lmy20020802@users.noreply.github.com>
2022-12-04 10:30:43 +0800

    4.实现分页模糊查询

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,10 @@
     <!--搜索表单-->
     <!---->
     <div style="margin-bottom: 20px">
-      <el-input placeholder="请输入名称" style="width: 240px;"></el-input>
-      <el-input placeholder="请输入联系方式" style="width: 240px;margin-left: 5px"></el-input>
-      <el-button style="margin-left: 5px" type="primary"><i class="el-icon-search"></i>搜索</el-button>
+      <el-input v-model="params.name" placeholder="请输入名称" style="width: 240px;"></el-input>
+      <el-input v-model="params.phone" placeholder="请输入联系方式" style="width: 240px;margin-left: 5px"></el-input>
+      <el-button style="margin-left: 5px" type="primary" @click="load"><i class="el-icon-search"></i>搜索</el-button>
+      <el-button style="margin-left: 5px" type="warning" @click="reset"><i class="el-icon-refresh"></i>重置</el-button>
     </div>
     <!--表格-->
     <el-table :data="tableData" stripe>
@@ -18,21 +19,32 @@
     <!--分页-->
     <div style="margin-top: 20px">
       <el-pagination
-          :total="100"
           background
-          layout="prev, pager, next">
+          :current-page="params.pageNum"
+          :page-size="params.pageSize"
+          :total="total"
+          layout="prev, pager, next"
+          @current-change="handleCurrentChange">
       </el-pagination>
     </div>
   </div>
 </template>
 
 <script>
+import request from "@/utils/request";
 
 export default {
   name: 'HomeView',
   data() {
     return {
-      tableData: []
+      tableData: [],
+      total: 0,
+      params: {
+        pageNum: 1,
+        pageSize: 2,
+        name: '',
+        phone: ''
+      }
     }
   },
   created() {
@@ -40,10 +52,33 @@ export default {
   },
   methods: {
     load() {
-      fetch('http://localhost:9090/user/list').then(res => res.json()).then(res => {
-        console.log(res)
-        this.tableData = res
+      // fetch('http://localhost:9090/user/list').then(res => res.json()).then(res => {
+      //   console.log(res)
+      //   this.tableData = res
+      // })
+
+      request.get('/user/page', {
+        params: this.params
+      }).then(res => {
+        if (res.code === '200') {
+          this.tableData = res.data.list
+          this.total = res.data.total
+        }
       })
+    },
+    handleCurrentChange(pageNum) {
+      // 点击分页按钮触发分页
+      this.params.pageNum = pageNum
+      this.load()
+    },
+    reset() {
+      this.params = {
+        pageNum: 1,
+        pageSize: 2,
+        name: '',
+        phone: ''
+      }
+      this.load()
     }
   }
 }
